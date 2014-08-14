69dbe5bee26f01c2bca4e5b6e5d082809ea2b117
Salnikova Marina <marishka.nika@gmail.com>
2014-08-14 13:54:11 +0700

    Update

---
+++ test/tests/exampleTest.php
@@ -0,0 +1,22 @@
+<?php
+class Test extends PHPUnit_Framework_TestCase
+{
+    public function testCityName()
+    {
+	$this->assertTrue(true);
+    }
+    public function test1ityName()
+    {
+	$this->assertTrue(true);
+    }
+    public function test2ityName()
+    {
+	$this->assertTrue(true);
+    }
+    public function test3CityName()
+    {
+	$this->assertTrue(true);
+    }
+
+}
+?>
---
+++ test/tests/test.php
@@ -0,0 +1,19 @@
+<?php
+class WebTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+    protected function setUp()
+    {
+    $this->setHost('localhost');
+    $this->setBrowser('chrome');
+        $this->setBrowserUrl('http://2gis.ru/');
+    }
+
+    public function testCityName()
+    {
+        $this->url('http://2gis.ru/novosibirsk');
+    $element = $this->byClassName('dashboard__cityNameLink');
+        $this->assertEquals('Новосибирск', $element->text());
+    }
+
+}
+?>
