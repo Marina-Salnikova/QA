189d89ed59695884dbfbcfa2b5aff40dd5b933ba
Salnikova Marina <marishka.nika@gmail.com>
2014-08-15 11:54:35 +0700

    Change tests

--- tests/acceptance/HomeCept.php
+++ tests/acceptance/HomeCept.php
@@ -3,8 +3,8 @@ $I = new WebGuy($scenario);
 $I->amOnPage('');
 //$I->see('Новосибирск');
 //$I->click('Аптеки');
-$I->fillField('//*[@id="module-1-1-1"]/div/input','Пиво');
-$I->click('//*[@id="module-1-1"]/div[2]/form/button[1]');
-$I->waitForText('Организации', 30);
-$I->see('Расширенный поиск');
+//$I->fillField('//*[@id="module-1-1-1"]/div/input','Пиво');
+//$I->click('//*[@id="module-1-1"]/div[2]/form/button[1]');
+//$I->waitForText('Организации', 30);
+//$I->see('Расширенный поиск');
 ?>
---
+++ tests/acceptance/MarkerClickCept.php
@@ -0,0 +1,12 @@
+<?php
+$I = new WebGuy($scenario);
+$I->amOnPage('');
+//$I->fillField('//*[@id="module-1-1-1"]/div/input','Пиво');
+//$I->click('//*[@id="module-1-1"]/div[2]/form/button[1]');
+//$marker='.//div[contains(@class,"leaflet-marker-pane")]/div[5]';
+//$I->waitForElement($marker, 10);
+//$I->click($marker);
+//$I->waitForElement('//*[@id="module-1-5-2-1"]');
+//$textFromeElement=$I->grabTextFrom('//*[@id="module-1-5-2-1"]');
+//$I->see($textFromeElement,'//*div[@class="searchResults__content"]');
+?>
