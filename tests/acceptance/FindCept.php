36369c6e452caf3b05ce30893d81ca4d5752aaad
Salnikova Marina <marishka.nika@gmail.com>
2014-08-18 14:21:26 +0700

    Change tests

--- tests/acceptance/FindCept.php
+++ tests/acceptance/FindCept.php
@@ -4,4 +4,6 @@ $I->amOnPage('');
 $I->fillField('.//*[@class="search__input"]','Москва');
 $I->click('.//*[@class="search__submit"]');
 $I->waitForElement($marker, 10);
+$I->click($marker);
+$I->waitForElement('.//div[@class="dg-popup__container"]',10);
 ?>
