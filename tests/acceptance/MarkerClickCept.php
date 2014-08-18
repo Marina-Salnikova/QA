f7584e73d709b82e3e8aff1bf2579ef1a89d9c01
Salnikova Marina <marishka.nika@gmail.com>
2014-08-18 13:59:29 +0700

    Change tests

--- tests/acceptance/MarkerClickCept.php
+++ tests/acceptance/MarkerClickCept.php
@@ -1,11 +1,11 @@
 <?php
 $I = new WebGuy($scenario);
 $I->amOnPage('');
-$I->fillField('.//*[@class="search__input"]','Москва');
-$I->click('.//*[@class="search__submit"]');
+$I->resizeWindow(800,900);
+$I->resizeWindow(900,1000);
 $I->waitForElement($marker, 10);
 $I->click($marker);
-$I->waitForElement('.//div[@class="dg-popup__container"]');
+$I->waitForElement('.//div[@class="dg-popup__container"]',10);
 //$textFromeElement=$I->grabTextFrom('//*[@id="module-1-5-2-1"]');
 //$I->see($textFromeElement,'//*div[@class="searchResults__content"]');
 ?>
