0daae692d5c0ca74f1b1968a73b777b3e7434a56
Salnikova Marina <marishka.nika@gmail.com>
2014-08-18 16:32:24 +0700

    Remove findCept from acceptance

--- tests/acceptance/FindCept.php
+++
@@ -1,9 +0,0 @@
-<?php
-$I = new WebGuy($scenario);
-$I->amOnPage('');
-$I->fillField('.//*[@class="search__input"]','Москва');
-$I->click('.//*[@class="search__submit"]');
-$I->waitForElement($marker, 10);
-$I->click($marker);
-$I->waitForElement('.//div[@class="dg-popup__container"]',10);
-?>
