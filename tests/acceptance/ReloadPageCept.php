78a52ae3ab29cf3b447960f3cb744938bd0e000e
Salnikova Marina <marishka.nika@gmail.com>
2014-08-20 16:34:42 +0700

    modified ReloadPageCept.php

--- tests/acceptance/ReloadPageCept.php
+++ tests/acceptance/ReloadPageCept.php
@@ -5,6 +5,5 @@ $I->fillField($search_field,'Москва');
 $I->click($submit);
 $I->waitForElement($marker, 10);
 $I->reloadPage();
-$I->see('Москва');
-$I->waitForElement($marker, 10);
+$I->waitForElement($marker, 5);
 ?>
