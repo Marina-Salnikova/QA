2f8913c5a899ed2507e94b71fa58d477bfc6e5bc
Salnikova Marina <marishka.nika@gmail.com>
2014-08-20 14:30:25 +0700

    Modifications

--- codception2.0/tests/api/_bootstrap.php
+++ codception2.0/tests/api/_bootstrap.php
@@ -3,3 +3,4 @@
 $area='/?r=weather/find&lon_top=82.560544&lat_top=55.174534&lon_bottom=83.318972&lat_bottom=54.843024';
 $city='/?r=weather/find&city=Novosibirsk&pr=ya';
 $coord='/?r=weather/find&lat=55.753676&lon=37.619899&pr=owm';
+?>
