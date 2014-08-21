c5a7710462eff4aca65f5eea480095447ac7a5b4
Salnikova Marina <marishka.nika@gmail.com>
2014-08-21 10:30:28 +0700

    modifications

--- tests/acceptance/ReloadPageCept.php
+++
@@ -1,9 +0,0 @@
-<?php
-$I = new WebGuy($scenario);
-$I->amOnPage('');
-$I->fillField($search_field,'Москва');
-$I->click($submit);
-$I->waitForElement($marker, 10);
-$I->reloadPage();
-$I->waitForElement($marker, 5);
-?>
