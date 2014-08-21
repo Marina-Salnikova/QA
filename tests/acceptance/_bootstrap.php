96889c28e0a8188c3e0f6f2706eb67bb79b9f178
Salnikova Marina <marishka.nika@gmail.com>
2014-08-21 16:29:53 +0700

    edit marker

--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 // Here you can initialize variables that will be available to your tests
-$marker='.//div[@class="marker clickable cluster"][1]';
-$marker2='.//div[@class="marker clickable cluster"][1]';
+$marker='.//*[@class="leaflet-marker-icon leaflet-zoom-animated leaflet-clickable"][1]';
+$marker2='.//*[@class="leaflet-marker-icon leaflet-zoom-animated leaflet-clickable"][1]';
 $search_field='.//*[@class="search__input"]';
 $submit='.//*[@class="search__submit"]';
 $weather_card='.//div[@class="card"]';
